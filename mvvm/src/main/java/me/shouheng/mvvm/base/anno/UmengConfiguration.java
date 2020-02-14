4fdd5389cb2565fa0d93488163c19b7d99392bae
WngShhng <shouheng2015@gmail.com>
2020-02-14 21:11:12 +0800

    fix: change umeng config

--- mvvm/src/main/java/me/shouheng/mvvm/base/anno/UmengConfiguration.java
+++ mvvm/src/main/java/me/shouheng/mvvm/base/anno/UmengConfiguration.java
@@ -35,6 +35,6 @@ public @interface UmengConfiguration {
      *
      * @return true if use umeng analytics
      */
-    boolean useUmengManual() default true;
+    boolean useUmengManual() default false;
 
 }
