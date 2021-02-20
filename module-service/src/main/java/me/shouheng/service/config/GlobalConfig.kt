5e3bd418640c277d7414c1c9fd9867c3405c5eae
Jeff Wang <shouheng2015@gmail.com>
2021-02-20 13:22:11 +0800

    feat: change the Resources

--- module-service/src/main/java/me/shouheng/service/config/GlobalConfig.kt
+++
@@ -1,8 +0,0 @@
-package me.shouheng.service.config
-
-import me.shouheng.vmlib.bean.Resources
-import me.shouheng.vmlib.bean.Status
-
-/** Is succeed */
-fun <T> Resources<T>.isSucceed() = this.status == Status.SUCCESS
-
