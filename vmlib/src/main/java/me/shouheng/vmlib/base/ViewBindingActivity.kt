9cf74b2f34958677e40095d8fe4a7da145ca29a3
Jeff Wang <shouheng2015@gmail.com>
2021-02-20 17:12:33 +0800

    feat： view binding class getter

--- vmlib/src/main/java/me/shouheng/vmlib/base/ViewBindingActivity.kt
+++ vmlib/src/main/java/me/shouheng/vmlib/base/ViewBindingActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.viewbinding.ViewBinding
 import me.shouheng.utils.stability.L
+import java.lang.IllegalStateException
 import java.lang.reflect.ParameterizedType
 
 abstract class ViewBindingActivity<U : BaseViewModel, T : ViewBinding> : BaseActivity<U>() {
@@ -15,7 +16,9 @@ abstract class ViewBindingActivity<U : BaseViewModel, T : ViewBinding> : BaseAct
     override fun getLayoutResId(): Int = -1
 
     override fun setupContentView(savedInstanceState: Bundle?) {
-        val vbClass: Class<T> = (this.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[1] as Class<T>
+        val vbClass: Class<T> = ((this.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments)
+            .firstOrNull { ViewBinding::class.java.isAssignableFrom(it as Class<*>) } as? Class<T>
+            ?: throw IllegalStateException("You must specify a view binding class.")
         val method = vbClass.getDeclaredMethod("inflate", LayoutInflater::class.java)
         try {
             binding = method.invoke(null, LayoutInflater.from(context)) as T
--- vmlib/src/main/java/me/shouheng/vmlib/base/ViewBindingFragment.kt
+++ vmlib/src/main/java/me/shouheng/vmlib/base/ViewBindingFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.viewbinding.ViewBinding
 import me.shouheng.utils.stability.L
+import java.lang.IllegalStateException
 import java.lang.reflect.ParameterizedType
 
 abstract class ViewBindingFragment<U : BaseViewModel, T : ViewBinding> : BaseFragment<U>() {
@@ -21,7 +22,9 @@ abstract class ViewBindingFragment<U : BaseViewModel, T : ViewBinding> : BaseFra
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val vbClass: Class<T> = (this.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[1] as Class<T>
+        val vbClass: Class<T> = ((this.javaClass.genericSuperclass as ParameterizedType).actualTypeArguments)
+            .firstOrNull { ViewBinding::class.java.isAssignableFrom(it as Class<*>) } as? Class<T>
+            ?: throw IllegalStateException("You must specify a view binding class.")
         val method = vbClass.getDeclaredMethod("inflate", LayoutInflater::class.java)
         try {
             binding = method.invoke(null, LayoutInflater.from(context)) as T
