d0f7db5f8df58f05c103ceea41c455617f9ed265
wangshouheng <wangshouheng@corp.netease.com>
2022-12-07 19:22:09 +0800

    rfct: container activity

--- vmlib/src/main/java/me/shouheng/vmlib/comn/ContainerActivity.java
+++ vmlib/src/main/java/me/shouheng/vmlib/comn/ContainerActivity.java
@@ -5,12 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
-import androidx.annotation.IdRes;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import me.shouheng.uix.pages.web.FragmentKeyDown;
 import me.shouheng.utils.app.ActivityUtils;
@@ -30,14 +26,11 @@ import me.shouheng.vmlib.base.BaseActivity;
  */
 public class ContainerActivity extends BaseActivity<EmptyViewModel> {
 
-    /** Global command handler. */
-    private static List<CommandHandler> commandHandlers = new CopyOnWriteArrayList<>();
-
     /** Key for the {@link Fragment} class used to create the fragment instance. */
     public static final String KEY_EXTRA_FRAGMENT_CLASS         = "__extra_key_fragment_class";
 
-    /** Key for the command. */
-    public static final String KEY_EXTRA_COMMAND                = "__extra_key_command";
+    /** Key for fragment providers. */
+    public static final String KEY_EXTRA_FRAGMENT_PROVIDER      = "__extra_key_fragment_provider";
 
     /** Key for theme of current activity. */
     public static final String KEY_EXTRA_THEME_ID               = "__extra_key_theme_id";
@@ -51,7 +44,6 @@ public class ContainerActivity extends BaseActivity<EmptyViewModel> {
     /**
      * Get a {@link ActivityUtils.Builder} object to build a request for container activity.
      * You can set argument of your fragment by the methods provided by the builder directly.
-     *
      * NOTE : THE BUNDLE FROM ACTIVITY INTENT WILL BE USED AS THE ARGUMENTS OF FRAGMENT.
      *
      * @param fragment  the fragment class
@@ -61,16 +53,6 @@ public class ContainerActivity extends BaseActivity<EmptyViewModel> {
      */
     public static <F extends Fragment> ActivityUtils.Builder<ContainerActivity> open(Class<F> fragment) {
         return ActivityUtils.open(ContainerActivity.class).put(KEY_EXTRA_FRAGMENT_CLASS, fragment);
-    }
-
-    /**
-     * Launch current activity with given command. The command will be handled by the
-     * already registered {@link CommandHandler} by {@link #registerCommandHandler(CommandHandler)}.
-     *
-     * @param command the command
-     */
-    public static ActivityUtils.Builder<ContainerActivity> open(int command) {
-        return ActivityUtils.open(ContainerActivity.class).put(KEY_EXTRA_COMMAND, command);
     }
 
     /**
@@ -87,24 +69,14 @@ public class ContainerActivity extends BaseActivity<EmptyViewModel> {
         return ActivityUtils.open(ContainerActivity.class).put(KEY_EXTRA_FRAGMENT_CLASS, fragment);
     }
 
-    /**
-     * Register a global command handler.
-     *
-     * @param handler the command handler.
-     */
-    public static void registerCommandHandler(CommandHandler handler) {
-        if (!commandHandlers.contains(handler)) {
-            commandHandlers.add(handler);
-        }
+    /** Launch container with fragment provider. */
+    public static <P extends FragmentProvider> ActivityUtils.Builder<ContainerActivity> openWith(Class<P> provider) {
+        return ActivityUtils.open(ContainerActivity.class).put(KEY_EXTRA_FRAGMENT_PROVIDER, provider);
     }
 
-    /**
-     * Unregister a global command handler
-     *
-     * @param handler the command handler
-     */
-    public static void unRegisterCommandHandler(CommandHandler handler) {
-        commandHandlers.remove(handler);
+    /** Launch container with fragment provider. */
+    public static <P extends AppFragmentProvider> ActivityUtils.Builder<ContainerActivity> openFragmentWith(Class<P> provider) {
+        return ActivityUtils.open(ContainerActivity.class).put(KEY_EXTRA_FRAGMENT_PROVIDER, provider);
     }
 
     @Override
@@ -130,49 +102,102 @@ public class ContainerActivity extends BaseActivity<EmptyViewModel> {
     protected void doCreateView(@Nullable Bundle savedInstanceState) {
         Intent intent = getIntent();
         Class fragmentClass = (Class) intent.getSerializableExtra(KEY_EXTRA_FRAGMENT_CLASS);
-        int command = intent.getIntExtra(KEY_EXTRA_COMMAND, Integer.MIN_VALUE);
-        // the bundle from activity intent will be used as the arguments of fragment
-        Bundle bundle = intent.getExtras();
-        try {
-            if (fragmentClass == null && command == Integer.MIN_VALUE) {
-                L.e("Error : fragmentClass is null.");
-                return;
+        Class providerClass = (Class) intent.getSerializableExtra(KEY_EXTRA_FRAGMENT_PROVIDER);
+        if (fragmentClass == null && providerClass == null) {
+            L.w("Warn: no fragment to add.");
+            return;
+        }
+        if (fragmentClass != null) {
+            instanceAndReplace(savedInstanceState, fragmentClass);
+        } else {
+            provideAndReplace(savedInstanceState, providerClass);
+        }
+    }
+
+    private void instanceAndReplace(@Nullable Bundle savedInstanceState, Class fragmentClass) {
+        if (Fragment.class.isAssignableFrom(fragmentClass)) {
+            // fix 2022-10-03 add new judge
+            instanceFragmentAndReplace(savedInstanceState, fragmentClass);
+        } else if (android.app.Fragment.class.isAssignableFrom(fragmentClass)) {
+            instanceAppFragmentAndReplace(savedInstanceState, fragmentClass);
+        }
+    }
+
+    /** Instance a fragment and add to container. */
+    private void instanceFragmentAndReplace(@Nullable Bundle savedInstanceState, Class fragmentClass) {
+        Bundle arguments = getIntent().getExtras();
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+        if (fragment == null || savedInstanceState == null || fragment.getClass() != fragmentClass) {
+            try {
+                fragment = (Fragment) fragmentClass.newInstance();
+                fragment.setArguments(arguments);
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .commit();
+            } catch (IllegalAccessException e) {
+                L.e(e);
+            } catch (InstantiationException e) {
+                L.e(e);
             }
-            if (fragmentClass != null) {
-                if (Fragment.class.isAssignableFrom(fragmentClass)) {
-                    // fix 2022-10-03 add new judge
-                    Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
-                    if (fragment == null || savedInstanceState == null || fragment.getClass() != fragmentClass) {
-                        fragment = (Fragment) fragmentClass.newInstance();
-                        fragment.setArguments(bundle);
-                        getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.container, fragment)
-                                .commit();
-                    }
-                } else if (android.app.Fragment.class.isAssignableFrom(fragmentClass)) {
-                    android.app.Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
-                    if (fragment == null || savedInstanceState == null || fragment.getClass() != fragmentClass) {
-                        fragment = (android.app.Fragment) fragmentClass.newInstance();
-                        fragment.setArguments(bundle);
-                        getFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.container, fragment)
-                                .commit();
-                    }
-                }
-            } else {
-                // handle command by command handler
-                for (CommandHandler handler : commandHandlers) {
-                    handler.handle(this, command, R.id.container, bundle);
+        }
+    }
+
+    /** Instance a app fragment and add to container. */
+    private void instanceAppFragmentAndReplace(@Nullable Bundle savedInstanceState, Class fragmentClass) {
+        Bundle arguments = getIntent().getExtras();
+        android.app.Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
+        if (fragment == null || savedInstanceState == null || fragment.getClass() != fragmentClass) {
+            try {
+                fragment = (android.app.Fragment) fragmentClass.newInstance();
+                fragment.setArguments(arguments);
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment)
+                        .commit();
+            } catch (IllegalAccessException e) {
+                L.e(e);
+            } catch (InstantiationException e) {
+                L.e(e);
+            }
+        }
+    }
+
+    private void provideAndReplace(@Nullable Bundle savedInstanceState, Class providerClass) {
+        if (FragmentProvider.class.isAssignableFrom(providerClass)) {
+            Bundle arguments = getIntent().getExtras();
+            Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.container);
+            if (fragment == null || savedInstanceState == null) {
+                try {
+                    FragmentProvider provider = (FragmentProvider) providerClass.newInstance();
+                    fragment = provider.get(this, arguments);
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, fragment)
+                            .commit();
+                } catch (IllegalAccessException e) {
+                    L.e(e);
+                } catch (InstantiationException e) {
+                    L.e(e);
                 }
             }
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-            L.d(e);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            L.d(e);
+        } else if (AppFragmentProvider.class.isAssignableFrom(providerClass)) {
+            Bundle arguments = getIntent().getExtras();
+            android.app.Fragment fragment = getFragmentManager().findFragmentById(R.id.container);
+            if (fragment == null || savedInstanceState == null) {
+                try {
+                    AppFragmentProvider provider = (AppFragmentProvider) providerClass.newInstance();
+                    fragment = provider.get(this, arguments);
+                    getFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.container, fragment)
+                            .commit();
+                } catch (IllegalAccessException e) {
+                    L.e(e);
+                } catch (InstantiationException e) {
+                    L.e(e);
+                }
+            }
         }
     }
 
@@ -220,19 +245,25 @@ public class ContainerActivity extends BaseActivity<EmptyViewModel> {
         void onBackPressed(ContainerActivity activity);
     }
 
-    /** Command handler callback */
-    public interface CommandHandler {
-
+    /** A provider for current fragment. */
+    public interface FragmentProvider {
         /**
-         * Method to handle the command.
+         * Method to get a fragment instance.
          *
-         * NOTE: THE COMMAND WITH VALUE {@link Integer#MIN_VALUE} WILL BE IGNORED!
-         *
-         * @param activity          the container activity
-         * @param intentExtras      the extras from {@link Activity#getIntent()}
-         * @param containerLayoutId container layout id
-         * @param command           command, you can use any integer except {@link Integer#MIN_VALUE}.
+         * @param activity the container activity
+         * @param intentExtras the extras from {@link Activity#getIntent()}
          */
-        void handle(ContainerActivity activity, int command, @IdRes int containerLayoutId, Bundle intentExtras);
+        Fragment get(ContainerActivity activity, Bundle intentExtras);
+    }
+
+    /** A provider for current fragment. */
+    public interface AppFragmentProvider {
+        /**
+         * Method to get a fragment instance.
+         *
+         * @param activity the container activity
+         * @param intentExtras the extras from {@link Activity#getIntent()}
+         */
+        android.app.Fragment get(ContainerActivity activity, Bundle intentExtras);
     }
 }
